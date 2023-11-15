c595aa95153348142de93c9c3424ef91ebb41dd4
Tkanno <38493521+yodaka0@users.noreply.github.com>
2023-11-15 13:56:53 +0900

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -20,7 +20,7 @@ def cpu_count() -> int:
 class MDetConfig:
     image_source: Path = MISSING
     model_path: Path = Path("models/md_v5a.0.0.pt")
-    threshold: float = 0.2
+    threshold: float = MISSING
     output_absolute_path: bool = True
     ncores: int = cpu_count()
     verbose: bool = False
